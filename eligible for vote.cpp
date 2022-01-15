f41806ff79f3f0429c7a53ad547978d1f13d589e
DivyanshiChouksey <89761026+DivyanshiChouksey@users.noreply.github.com>
2022-01-15 22:54:00 +0530

    Create eligible for vote.cpp

---
+++ eligible for vote.cpp
@@ -0,0 +1,31 @@
+// wap of eligible for vote
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+
+{
+
+	int age = 0;
+
+	cout<<"Enter your age ";
+
+	cin>>age;
+
+	if (age>=18){
+
+		cout<<"you can vote";
+
+	}
+
+	else{
+
+		cout<<"you can't vote";
+
+	}
+
+	return 0;
+
+}
