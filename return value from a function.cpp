13d8d53c08cf73fd39eee834c624353cee420581
DivyanshiChouksey <89761026+DivyanshiChouksey@users.noreply.github.com>
2022-01-22 00:32:20 +0530

    Create return value from a function.cpp

---
+++ return value from a function.cpp
@@ -0,0 +1,12 @@
+//WAP to return value from a function
+#include<iostream>
+using namespace std;
+
+int area(int l, int b){
+	return l*b;
+}
+int main(){
+	int a=area(4,2);
+	cout<<a;
+	return 0;
+}
