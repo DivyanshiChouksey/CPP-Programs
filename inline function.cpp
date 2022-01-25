4c120e82c93b2c7c45071bbd46d1ad1bb81cc870
DivyanshiChouksey <89761026+DivyanshiChouksey@users.noreply.github.com>
2022-01-25 12:52:37 +0530

    Create inline function.cpp

---
+++ inline function.cpp
@@ -0,0 +1,11 @@
+//inline function
+#include<iostream>
+using namespace std;
+
+inline int add(int a, int b){
+	return (a+b);
+}
+int main(){
+	cout<<"addition of 'a' & 'b' is "<<add(2,3);
+	return 0;
+}
